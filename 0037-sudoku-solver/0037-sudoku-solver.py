cbe882b73838c1a522079ca3c7246814e0231b71
Dhivya <128796577+dhivyakalpana@users.noreply.github.com>
2025-08-07 22:54:06 +0530

    Time: 1595 ms (58.73%), Space: 18.2 MB (5.96%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -14,19 +14,22 @@ class Solution:
                     boxes[box_id].add(num)
         
         solved=False
-
         def backtrack(i:int,j:int):
             nonlocal solved
             if solved:
                 return 
+            
             if i==9:
                 solved=True
-                return
-            ni,nj=(i,j+1) if j<8 else(i+1,0)
-            if board[i][j] != ".":
+                return 
+            
+
+            ni,nj=(i,j+1) if j<8 else (i+1,0)
+            if board[i][j]!=".":
                 backtrack(ni,nj)
             else:
                 box_id=(i//3)*3+(j//3)
+
                 for num in range(1,10):
                     if num not in rows[i] and num not in cols[j] and num not in boxes[box_id]:
                         board[i][j]=str(num)
@@ -34,10 +37,15 @@ class Solution:
                         cols[j].add(num)
                         boxes[box_id].add(num)
                         backtrack(ni,nj)
+
                         if solved:
-                            return
+                            return 
+
                         board[i][j]="."
                         rows[i].remove(num)
                         cols[j].remove(num)
                         boxes[box_id].remove(num)
+            
         backtrack(0,0)
+
+            
